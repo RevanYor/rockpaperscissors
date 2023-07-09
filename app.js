fa30b8816cf5f6038b244ccb05c137086e786f2c
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-07-08 21:52:45 -0400

    stuff

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
 const arr = ['rock','paper','scissor'];
-/* const user = prompt().toLowerCase(); */
-
-/* function getComputer() {
-    const choice = arr[(Math.floor(Math.random() * arr.length))];
-    return choice;
-}
-const comp = getComputer(); */
-
-/* function play(user,comp) {
-    if ((user === 'rock') && (comp === 'scissor')) {
-        console.log('win');
-    } else if ((user ==='paper') && (comp === 'rock')) {
-        console.log('win');
-    } else if ((user ==='scissor') && (comp === 'paper')) {
-        console.log('win');
-    } else if ((comp === 'rock') && (user === 'scissor')) {
-        console.log('lose');
-    } else if ((comp ==='paper') && (user === 'rock')) {
-        console.log('lose');
-    } else if ((comp ==='scissor') && (user === 'paper')) {
-        console.log('lose');
-    } else if (user === comp) {
-        console.log('tie');
-    } else {
-        console.log('nope');
-    }
-} */
 
 function game() {
 
@@ -72,6 +45,4 @@ function game() {
         play(user,comp);
     }  
 }
-
 console.log(game());
-//play();
